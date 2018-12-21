bc45387487fd490450600b8c9a3284770193367b
Pierre Kestener <pierre.kestener@cea.fr>
2018-12-21 23:42:01 +0100

    minor

--- src/PABLO/Intersection.hpp
+++ src/PABLO/Intersection.hpp
@@ -20,25 +20,25 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
  *
-\*---------------------------------------------------------------------------*/
+ \*---------------------------------------------------------------------------*/
 
 #ifndef __BITPIT_PABLO_INTERSECTION_HPP__
 #define __BITPIT_PABLO_INTERSECTION_HPP__
 
-// =================================================================================== //
-// INCLUDES                                                                            //
-// =================================================================================== //
+// ======================================================================== //
+// INCLUDES
+// ======================================================================== //
 #include <stdint.h>
 
 namespace bitpit {
 
-// =================================================================================== //
-// NAME SPACES                                                                         //
-// =================================================================================== //
+// ======================================================================== //
+// NAME SPACES
+// ======================================================================== //
 
-// =================================================================================== //
-// CLASS DEFINITION                                                                    //
-// =================================================================================== //
+// ======================================================================== //
+// CLASS DEFINITION
+// ======================================================================== //
 /*!
  *	\ingroup		PABLO
  *	\date			16/dec/2015
@@ -59,64 +59,64 @@ namespace bitpit {
  *	- a flag to communicate if the intersection is new after a mesh refinement.
  *
  */
-class Intersection{
+class Intersection {
 
-	// =================================================================================== //
-	// FRIENDSHIPS
-	// =================================================================================== //
+  // ====================================================================== //
+  // FRIENDSHIPS
+  // ====================================================================== //
 
-	friend class LocalTree;
-	friend class ParaTree;
+  friend class LocalTree;
+  friend class ParaTree;
 
-	// =================================================================================== //
-	// TYPEDEFS
-	// =================================================================================== //
+  // ======================================================================= //
+  // TYPEDEFS
+  // ======================================================================= //
 
-	// =================================================================================== //
-	// MEMBERS
-	// =================================================================================== //
+  // ======================================================================= //
+  // MEMBERS
+  // ======================================================================= //
 private:
-	uint32_t 	m_owners[2];		/**< Owner octants of the intersection (first is the internal octant) */
-	uint8_t   	m_iface;			/**< Index of the face of the outer owner */
-	bool		m_out;				/**< 0/1 octant with exiting normal (if boundary =0) */
-	bool		m_outisghost;		/**< 0/1 if octant with exiting normal is a ghost octant */
-	bool		m_finer;			/**< 0/1 finer octant (if same level =0) */
-	bool		m_isghost;			/**< The intersection has a member ghost */
-	bool		m_isnew;			/**< The intersection is new after a mesh adapting? */
-	bool		m_bound;			/**< The intersection is a boundary intersection of the whole domain */
-	bool		m_pbound;			/**< The intersection is a boundary intersection of a process domain */
-	uint8_t		m_dim;				/**< Dimension of intersection (2D/3D) */
+  uint32_t 	m_owners[2];	/**< Owner octants of the intersection (first is the internal octant) */
+  uint8_t   	m_iface;	/**< Index of the face of the outer owner */
+  bool		m_out;		/**< 0/1 octant with exiting normal (if boundary =0) */
+  bool		m_outisghost;	/**< 0/1 if octant with exiting normal is a ghost octant */
+  bool		m_finer;	/**< 0/1 finer octant (if same level =0) */
+  bool		m_isghost;	/**< The intersection has a member ghost */
+  bool		m_isnew;	/**< The intersection is new after a mesh adapting? */
+  bool		m_bound;	/**< The intersection is a boundary intersection of the whole domain */
+  bool		m_pbound;	/**< The intersection is a boundary intersection of a process domain */
+  uint8_t		m_dim;	/**< Dimension of intersection (2D/3D) */
 
-	// =================================================================================== //
-	// CONSTRUCTORS AND OPERATORS
-	// =================================================================================== //
+  // ====================================================================== //
+  // CONSTRUCTORS AND OPERATORS
+  // ====================================================================== //
 public:
-	Intersection();
-	Intersection(const Intersection & intersection);
-	Intersection & operator =(const Intersection & intersection);
+  Intersection();
+  Intersection(const Intersection & intersection);
+  Intersection & operator =(const Intersection & intersection);
 private:
-	Intersection(uint8_t dim);
-	bool operator ==(const Intersection & intersection);
+  Intersection(uint8_t dim);
+  bool operator ==(const Intersection & intersection);
+  
+  // ====================================================================== //
+  // METHODS
+  // ====================================================================== //
 
-	// =================================================================================== //
-	// METHODS
-	// =================================================================================== //
+  // ====================================================================== //
+  // BASIC GET/SET METHODS
+  // ====================================================================== //
 
-	// =================================================================================== //
-	// BASIC GET/SET METHODS
-	// =================================================================================== //
+  uint32_t getOut() const;
+  bool getOutIsGhost() const;
+  uint32_t getIn() const;
+  uint32_t getFiner() const;
+  void getNormal(int8_t normal[3], int8_t normals[6][3]) const;
+  bool getBound() const;
+  bool getIsGhost() const;
+  bool getPbound() const;
 
-	uint32_t getOut() const;
-	bool getOutIsGhost() const;
-	uint32_t getIn() const;
-	uint32_t getFiner() const;
-	void getNormal(int8_t normal[3], int8_t normals[6][3]) const;
-	bool getBound() const;
-	bool getIsGhost() const;
-	bool getPbound() const;
+}; // class Intersection
 
-};
-
-}
+} // namespace bitpit
 
 #endif /* __BITPIT_PABLO_INTERSECTION_HPP__ */
--- src/PABLO/morton.hpp
+++ src/PABLO/morton.hpp
@@ -20,7 +20,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
  *
-\*---------------------------------------------------------------------------*/
+ \*---------------------------------------------------------------------------*/
 
 #ifndef __BITPIT_PABLO_MORTON_HPP__
 #define __BITPIT_PABLO_MORTON_HPP__
@@ -30,113 +30,113 @@ namespace bitpit {
 namespace PABLO {
 
 /**
-* Seperate bits from a given integer 3 positions apart.
-*
-* The function comes from libmorton (see https://github.com/Forceflow/libmorton).
-*
-* \param a is an integer position
-* \result Separated bits.
-*/
+ * Seperate bits from a given integer 3 positions apart.
+ *
+ * The function comes from libmorton (see https://github.com/Forceflow/libmorton).
+ *
+ * \param a is an integer position
+ * \result Separated bits.
+ */
 inline uint64_t splitBy3(uint32_t a)
 {
-    uint64_t x = a & 0x1fffff; // we only look at the first 21 bits
-    x = (x | x << 32) & 0x1f00000000ffff;  // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
-    x = (x | x << 16) & 0x1f0000ff0000ff;  // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
-    x = (x | x << 8) & 0x100f00f00f00f00f; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
-    x = (x | x << 4) & 0x10c30c30c30c30c3; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
-    x = (x | x << 2) & 0x1249249249249249;
+  uint64_t x = a & 0x1fffff; // we only look at the first 21 bits
+  x = (x | x << 32) & 0x1f00000000ffff;  // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
+  x = (x | x << 16) & 0x1f0000ff0000ff;  // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
+  x = (x | x << 8) & 0x100f00f00f00f00f; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
+  x = (x | x << 4) & 0x10c30c30c30c30c3; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
+  x = (x | x << 2) & 0x1249249249249249;
 
-    return x;
+  return x;
 }
 
 /**
-* Seperate bits from a given integer 2 positions apart.
-*
-* The function comes from libmorton (see https://github.com/Forceflow/libmorton).
-*
-* \param a is an integer position
-* \result Separated bits.
-*/
+ * Seperate bits from a given integer 2 positions apart.
+ *
+ * The function comes from libmorton (see https://github.com/Forceflow/libmorton).
+ *
+ * \param a is an integer position
+ * \result Separated bits.
+ */
 inline uint64_t splitBy2(uint32_t a)
 {
-    uint64_t x = a;
-    x = (x | x << 16) & 0xFFFF0000FFFF;  // shift left 16 bits, OR with self, and 0000000000000000111111111111111100000000000000001111111111111111
-    x = (x | x << 8) & 0xFF00FF00FF00FF;  // shift left 8 bits, OR with self, and 0000000011111111000000001111111100000000111111110000000011111111
-    x = (x | x << 4) & 0xF0F0F0F0F0F0F0F; // shift left 4 bits, OR with self, and 0000111100001111000011110000111100001111000011110000111100001111
-    x = (x | x << 2) & 0x3333333333333333; // shift left 2 bits, OR with self, and 0011001100110011001100110011001100110011001100110011001100110011
-    x = (x | x << 1) & 0x5555555555555555; // shift left 1 bits, OR with self, and 0101010101010101010101010101010101010101010101010101010101010101
+  uint64_t x = a;
+  x = (x | x << 16) & 0xFFFF0000FFFF;  // shift left 16 bits, OR with self, and 0000000000000000111111111111111100000000000000001111111111111111
+  x = (x | x << 8) & 0xFF00FF00FF00FF;  // shift left 8 bits, OR with self, and 0000000011111111000000001111111100000000111111110000000011111111
+  x = (x | x << 4) & 0xF0F0F0F0F0F0F0F; // shift left 4 bits, OR with self, and 0000111100001111000011110000111100001111000011110000111100001111
+  x = (x | x << 2) & 0x3333333333333333; // shift left 2 bits, OR with self, and 0011001100110011001100110011001100110011001100110011001100110011
+  x = (x | x << 1) & 0x5555555555555555; // shift left 1 bits, OR with self, and 0101010101010101010101010101010101010101010101010101010101010101
 
-    return x;
+  return x;
 }
 
 /**
-* Compute the Morton number of the given set of coordinates.
-*
-* The function uses the "magic bits" algorithm of the libmorton library
-* (see https://github.com/Forceflow/libmorton).
-*
-* \param x is the integer x position
-* \param y is the integer y position
-* \param z is the integer z position
-* \result The Morton number.
-*/
+ * Compute the Morton number of the given set of coordinates.
+ *
+ * The function uses the "magic bits" algorithm of the libmorton library
+ * (see https://github.com/Forceflow/libmorton).
+ *
+ * \param x is the integer x position
+ * \param y is the integer y position
+ * \param z is the integer z position
+ * \result The Morton number.
+ */
 inline uint64_t computeMorton(uint32_t x, uint32_t y, uint32_t z)
 {
-    uint64_t morton = splitBy3(x) | splitBy3(y) << 1 | splitBy3(z) << 2;
+  uint64_t morton = splitBy3(x) | splitBy3(y) << 1 | splitBy3(z) << 2;
 
-    return morton;
+  return morton;
 }
 
 /**
-* Compute the Morton number of the given set of coordinates.
-*
-* The function uses the "magic bits" algorithm of the libmorton library
-* (see https://github.com/Forceflow/libmorton).
-*
-* \param x is the integer x position
-* \param y is the integer y position
-* \result The Morton number.
-*/
+ * Compute the Morton number of the given set of coordinates.
+ *
+ * The function uses the "magic bits" algorithm of the libmorton library
+ * (see https://github.com/Forceflow/libmorton).
+ *
+ * \param x is the integer x position
+ * \param y is the integer y position
+ * \result The Morton number.
+ */
 inline uint64_t computeMorton(uint32_t x, uint32_t y)
 {
-    uint64_t morton = splitBy2(x) | splitBy2(y) << 1;
+  uint64_t morton = splitBy2(x) | splitBy2(y) << 1;
 
-    return morton;
+  return morton;
 }
 
 /**
-* Compute the XYZ key of the given set of coordinates.
-*
-* \param x is the integer x position
-* \param y is the integer y position
-* \param z is the integer z position
-* \param maxLevel is the Maximum allowed refinement level of octree
-* \result The Morton number.
-*/
+ * Compute the XYZ key of the given set of coordinates.
+ *
+ * \param x is the integer x position
+ * \param y is the integer y position
+ * \param z is the integer z position
+ * \param maxLevel is the Maximum allowed refinement level of octree
+ * \result The Morton number.
+ */
 inline uint64_t computeXYZKey(uint64_t x, uint64_t y, uint64_t z, int8_t maxLevel)
 {
-    uint64_t key = x | (y << (maxLevel+1)) | (z << 2*(maxLevel+1));
+  uint64_t key = x | (y << (maxLevel+1)) | (z << 2*(maxLevel+1));
 
-    return key;
+  return key;
 }
 
 /**
-* Compute the XYZ key number of the given set of coordinates.
-*
-* \param x is the integer x position
-* \param y is the integer y position
-* \param maxLevel is the Maximum allowed refinement level of octree
-* \result The Morton number.
-*/
+ * Compute the XYZ key number of the given set of coordinates.
+ *
+ * \param x is the integer x position
+ * \param y is the integer y position
+ * \param maxLevel is the Maximum allowed refinement level of octree
+ * \result The Morton number.
+ */
 inline uint64_t computeXYZKey(uint64_t x, uint64_t y, int8_t maxLevel)
 {
-    uint64_t key = x | (y << (maxLevel+1));
+  uint64_t key = x | (y << (maxLevel+1));
 
-    return key;
+  return key;
 }
 
-}
+} // namespace PABLO
 
-}
+} // namespace bitpit
 
 #endif
