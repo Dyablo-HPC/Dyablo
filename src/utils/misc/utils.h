b06d43a33d053826ca6b0e9b918f981b1db0b05a
Pierre Kestener <pierre.kestener@cea.fr>
2019-01-15 14:55:39 +0100

    make sure src/utils builds fine

---
+++ src/utils/misc/utils.h
@@ -0,0 +1,9 @@
+#ifndef KHAMR_UTILS_H
+#define KHAMR_UTILS_H
+
+#include <iosfwd>
+
+void        print_current_date(std::ostream& stream);
+std::string get_current_date();
+
+#endif // KHAMR_UTILS_H
