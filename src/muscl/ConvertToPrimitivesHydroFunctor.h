5fe33581a53169240c68dc3c9ebfe2ce0d315c35
Pierre Kestener <pierre.kestener@cea.fr>
2019-02-11 14:07:27 +0100

    add ConvertToPrimitivesHydroFunctor implem

--- src/muscl/ConvertToPrimitivesHydroFunctor.h
+++ src/muscl/ConvertToPrimitivesHydroFunctor.h
@@ -51,12 +51,68 @@ public:
   }
 
   KOKKOS_INLINE_FUNCTION
+  void operator_2d(const size_t& i) const
+  {
+    
+    HydroState2d uLoc; // conservative variables in current cell
+    HydroState2d qLoc; // primitive    variables in current cell
+    real_t c = 0.0;
+    
+    // get local conservative variable
+    uLoc[ID] = Udata(i,fm[ID]);
+    uLoc[IP] = Udata(i,fm[IP]);
+    uLoc[IU] = Udata(i,fm[IU]);
+    uLoc[IV] = Udata(i,fm[IV]);
+    
+    // get primitive variables in current cell
+    computePrimitives(uLoc, &c, qLoc);
+    
+    // copy q state in q global
+    Qdata(i,fm[ID]) = qLoc[ID];
+    Qdata(i,fm[IP]) = qLoc[IP];
+    Qdata(i,fm[IU]) = qLoc[IU];
+    Qdata(i,fm[IV]) = qLoc[IV];
+
+  } // operator_2d
+
+  KOKKOS_INLINE_FUNCTION
+  void operator_3d(const size_t& i) const
+  {
+    
+    HydroState3d uLoc; // conservative variables in current cell
+    HydroState3d qLoc; // primitive    variables in current cell
+    real_t c = 0.0;
+    
+    // get local conservative variable
+    uLoc[ID] = Udata(i,fm[ID]);
+    uLoc[IP] = Udata(i,fm[IP]);
+    uLoc[IU] = Udata(i,fm[IU]);
+    uLoc[IV] = Udata(i,fm[IV]);
+    uLoc[IW] = Udata(i,fm[IW]);
+    
+    // get primitive variables in current cell
+    computePrimitives(uLoc, &c, qLoc);
+    
+    // copy q state in q global
+    Qdata(i,fm[ID]) = qLoc[ID];
+    Qdata(i,fm[IP]) = qLoc[IP];
+    Qdata(i,fm[IU]) = qLoc[IU];
+    Qdata(i,fm[IV]) = qLoc[IV];
+    Qdata(i,fm[IW]) = qLoc[IW];
+
+  } // operator_3d
+
+  KOKKOS_INLINE_FUNCTION
   void operator()(const size_t& i) const
   {
-
-    // TODO !!!
-  }
-
+    
+    if (this->params.dimType == TWO_D)
+      operator_2d(i);
+    
+    if (this->params.dimType == THREE_D)
+      operator_3d(i);
+    
+  } // operator ()
   
   std::shared_ptr<AMRmesh> pmesh;
   HydroParams  params;
