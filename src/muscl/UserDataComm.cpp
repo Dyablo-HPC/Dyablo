b988477446aca3b1a9bd3145b6caecb0143025fe
Pierre Kestener <pierre.kestener@cea.fr>
2019-02-25 15:06:51 +0100

    add simple UserDataComm class for ghost data synchronizing in muscl scheme

---
+++ src/muscl/UserDataComm.cpp
@@ -0,0 +1,47 @@
+/*
+ * UserDataComm.cpp
+ *
+ */
+
+#include "muscl/UserDataComm.h"
+
+namespace euler_pablo { namespace muscl {
+
+// ==================================================================
+// ==================================================================
+UserDataComm::UserDataComm(DataArray data_, DataArray ghostData_) :
+  data(data_),
+  ghostData(ghostData_),
+  nbVars(data_.dimension(1))
+{
+}; // UserDataComm::UserDataComm
+
+// ==================================================================
+// ==================================================================
+UserDataComm::~UserDataComm()
+{
+}; // UserDataComm::~UserDataComm
+
+// ==================================================================
+// ==================================================================
+inline size_t UserDataComm::fixedSize() const
+{
+  
+  return 0;
+  
+}; // UserDataComm::fixedSize
+
+// ==================================================================
+// ==================================================================
+inline size_t UserDataComm::size(const uint32_t e) const
+{
+  
+  BITPIT_UNUSED(e);
+  
+  return sizeof(real_t)*nbVars;
+  
+}; // UserDataComm::size
+
+} // namespace muscl
+
+} // namespace euler_pablo
---
+++ src/muscl/UserDataComm.h
@@ -0,0 +1,73 @@
+
+/*
+ * UserDataComm.h for muscl scheme.
+ *
+ */
+#ifndef MUSCL_USER_DATA_COMM_H_
+#define MUSCL_USER_DATA_COMM_H_
+
+#include "bitpit_PABLO.hpp" // for DataCommInterface
+#include "shared/kokkos_shared.h" // for type DataArray
+
+namespace euler_pablo { namespace muscl {
+
+
+/**
+ * Main class for user data communication.
+ */
+class UserDataComm : public bitpit::DataCommInterface<UserDataComm>
+{
+
+public:
+  
+  DataArray data;
+  DataArray ghostData;
+
+  uint32_t nbVars;
+  
+  size_t fixedSize() const;
+  size_t size(const uint32_t e) const;
+
+  /**
+   * read data to communicate to neighbor MPI processes.
+   */
+  template<class Buffer>
+  void gather(Buffer & buff, const uint32_t e) {
+
+    for (uint32_t ivar=0; ivar<nbVars; ++ivar) {
+      buff << data(e,ivar);
+      buff << data(e,ivar);
+    }
+
+  } // gather
+
+  /**
+   * Fill ghosts with data received from neighbor MPI processes.
+   */
+  template<class Buffer>
+  void scatter(Buffer & buff, const uint32_t e) {
+
+    for (uint32_t ivar=0; ivar<nbVars; ++ivar) {
+      buff >> ghostData(e,ivar);
+      buff >> ghostData(e,ivar);
+    }
+
+  } // scatter
+
+  /**
+   * Constructor.
+   */
+  UserDataComm(DataArray data_, DataArray ghostData_);
+
+  /**
+   * Destructor.
+   */
+  ~UserDataComm();
+  
+}; // class UserDataComm
+
+} // namespace muscl
+
+} // namespace euler_pablo
+
+#endif // MUSCL_USER_DATA_COMM_H_
