b3baf300020685a9432807d8767e8c77e251e5b3
Pierre Kestener <pierre.kestener@cea.fr>
2019-02-27 18:07:23 +0100

    add class for load balancing user data

---
+++ src/muscl/UserDataLB.cpp
@@ -0,0 +1,88 @@
+/*
+ * UserDataLB.cpp
+ *
+ */
+
+#include "muscl/UserDataLB.h"
+
+#include "bitpit_common.hpp"
+
+namespace euler_pablo { namespace muscl {
+
+// ==================================================================
+// ==================================================================
+UserDataLB::UserDataLB(DataArray data_, DataArray ghostdata_) :
+  data(data_),
+  ghostdata(ghostdata_),
+  nbVars(data_.dimension(1))
+{
+};
+
+// ==================================================================
+// ==================================================================
+UserDataLB::~UserDataLB()
+{
+};
+
+// ==================================================================
+// ==================================================================
+size_t UserDataLB::fixedSize() const
+{
+  return 0;
+};
+
+// ==================================================================
+// ==================================================================
+size_t UserDataLB::size(const uint32_t e) const
+{
+  BITPIT_UNUSED(e);
+  return sizeof(double)*nbVars;
+};
+
+// ==================================================================
+// ==================================================================
+void UserDataLB::move(const uint32_t from, const uint32_t to)
+{
+  for (uint32_t ivar=0; ivar<nbVars; ++ivar)
+    data(to,ivar) = data(from,ivar);
+};
+
+// ==================================================================
+// ==================================================================
+void UserDataLB::assign(uint32_t stride, uint32_t length)
+{
+  
+  DataArray dataCopy("dataLBcopy");
+  Kokkos::resize(dataCopy,length,nbVars);
+  
+  Kokkos::parallel_for(length, KOKKOS_LAMBDA(size_t &i) {
+      for (uint32_t ivar=0; ivar<nbVars; ++ivar)
+	dataCopy(i,ivar) = data(i+stride,ivar);
+    });
+  
+  data = dataCopy;
+};
+
+// ==================================================================
+// ==================================================================
+void UserDataLB::resize(uint32_t newSize)
+{
+  Kokkos::resize(data,newSize,nbVars);
+};
+
+// ==================================================================
+// ==================================================================
+void UserDataLB::resizeGhost(uint32_t newSize)
+{
+  Kokkos::resize(ghostdata,newSize,nbVars);
+};
+
+// ==================================================================
+// ==================================================================
+void UserDataLB::shrink() {
+  // TODO ?
+};
+
+} // namespace muscl
+
+} // namespace euler_pablo
---
+++ src/muscl/UserDataLB.h
@@ -0,0 +1,63 @@
+/*
+ * UserDataLB.h for muscl scheme.
+ *
+ * MPI load balancing user data.
+ */
+#ifndef MUSCL_USER_DATA_LB_H_
+#define MUSCL_USER_DATA_LB_H_
+
+#include <cstdlib>
+#include <cstdint>
+
+#include "DataLBInterface.hpp" // for DataCommInterface
+#include "shared/kokkos_shared.h"
+
+namespace euler_pablo { namespace muscl {
+
+/**
+ * Main class for user data load balancing.
+ */
+class UserDataLB : public bitpit::DataLBInterface<UserDataLB>
+{
+
+public:
+  
+  // pass by copy (Kokkos::View), watchout data and ghostdata
+  // wii surely be reassigned, so calling code must be aware of
+  // that.
+  DataArray data;
+  DataArray ghostdata;
+
+  uint32_t nbVars;
+  
+  size_t fixedSize() const;
+  size_t size(const uint32_t e) const;
+  void move(const uint32_t from, const uint32_t to);
+
+  template<class Buffer>
+  void gather(Buffer & buff, const uint32_t e) {
+    for (uint32_t ivar=0; ivar<nbVars; ++ivar)
+      buff << data(e,ivar);
+  };
+
+  template<class Buffer>
+  void scatter(Buffer & buff, const uint32_t e) {
+    for (uint32_t ivar=0; ivar<nbVars; ++ivar)
+      buff >> data(e,ivar);
+  };
+
+  void assign(uint32_t stride, uint32_t length);
+  void resize(uint32_t newSize);
+  void resizeGhost(uint32_t newSize);
+  void shrink();
+
+  UserDataLB(DataArray data_, DataArray ghostdata_);
+  ~UserDataLB();
+  
+}; // class UserDataLb
+
+} // namespace muscl
+
+} // namespace euler_pablo
+
+#endif // MUSCL_USER_DATA_LB_H_
