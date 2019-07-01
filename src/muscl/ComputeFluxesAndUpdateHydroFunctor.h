4182d67e74d72253a9f757072d7fd36de30ba35a
Pierre Kestener <pierre.kestener@cea.fr>
2019-07-01 15:55:37 +0200

    update ComputeFluxesAndUpdateHydroFunctor for 3d

--- src/muscl/ComputeFluxesAndUpdateHydroFunctor.h
+++ src/muscl/ComputeFluxesAndUpdateHydroFunctor.h
@@ -516,6 +516,118 @@ public:
 
   // =======================================================================
   // =======================================================================
+  /**
+   * Reconstruct an hydro state at a cell border location specified by offsets (3d version).
+   *
+   * \sa reconstruct_state_2d
+   */
+  KOKKOS_INLINE_FUNCTION
+  HydroState3d reconstruct_state_3d(HydroState3d q, 
+                                    uint32_t i,
+                                    bool isghost,
+                                    offsets_t offsets,
+                                    real_t dx_over_2, real_t dt) const
+  {
+    HydroState3d qr;
+    
+    const double gamma  = params.settings.gamma0;
+    const double smallr = params.settings.smallr;
+    
+    //double xyz_center[3];
+    
+    real_t r,p,u,v,w;
+    real_t sr0, sp0, su0, sv0, sw0;
+    real_t drx, dpx, dux, dvx, dwx;
+    real_t dry, dpy, duy, dvy, dwy;
+    real_t drz, dpz, duz, dvz, dwz;
+    
+    const real_t dtdx = dt/(2*dx_over_2);
+    const real_t dtdy = dtdx;
+    const real_t dtdz = dtdx;
+
+    real_t &dx2 = dx_over_2;
+    real_t &dy2 = dx_over_2;
+    real_t &dz2 = dx_over_2;
+
+    // retrieve primitive variables in current quadrant
+    r = q[ID];
+    p = q[IP];
+    u = q[IU];
+    v = q[IV];
+    w = q[IW];
+
+    // retrieve variations = dx * slopes
+    if (isghost) {
+
+      drx = dx2 * Slopes_x_ghost(i, fm[ID]);
+      dpx = dx2 * Slopes_x_ghost(i, fm[IP]);
+      dux = dx2 * Slopes_x_ghost(i, fm[IU]);
+      dvx = dx2 * Slopes_x_ghost(i, fm[IV]);
+      dwx = dx2 * Slopes_x_ghost(i, fm[IW]);
+
+      dry = dy2 * Slopes_y_ghost(i, fm[ID]);
+      dpy = dy2 * Slopes_y_ghost(i, fm[IP]);
+      duy = dy2 * Slopes_y_ghost(i, fm[IU]);
+      dvy = dy2 * Slopes_y_ghost(i, fm[IV]);
+      dwy = dy2 * Slopes_y_ghost(i, fm[IW]);
+
+      drz = dz2 * Slopes_z_ghost(i, fm[ID]);
+      dpz = dz2 * Slopes_z_ghost(i, fm[IP]);
+      duz = dz2 * Slopes_z_ghost(i, fm[IU]);
+      dvz = dz2 * Slopes_z_ghost(i, fm[IV]);
+      dwz = dz2 * Slopes_z_ghost(i, fm[IW]);
+
+    } else {
+
+      drx = dx2 * Slopes_x(i, fm[ID]);
+      dpx = dx2 * Slopes_x(i, fm[IP]);
+      dux = dx2 * Slopes_x(i, fm[IU]);
+      dvx = dx2 * Slopes_x(i, fm[IV]);
+      dwx = dx2 * Slopes_x(i, fm[IW]);
+
+      dry = dy2 * Slopes_y(i, fm[ID]);
+      dpy = dy2 * Slopes_y(i, fm[IP]);
+      duy = dy2 * Slopes_y(i, fm[IU]);
+      dvy = dy2 * Slopes_y(i, fm[IV]);
+      dwy = dy2 * Slopes_y(i, fm[IW]);
+
+      drz = dz2 * Slopes_z(i, fm[ID]);
+      dpz = dz2 * Slopes_z(i, fm[IP]);
+      duz = dz2 * Slopes_z(i, fm[IU]);
+      dvz = dz2 * Slopes_z(i, fm[IV]);
+      dwz = dz2 * Slopes_z(i, fm[IW]);
+
+    }
+
+    // source terms (with transverse derivatives)
+    sr0 = (-u*drx-dux*r      )*dtdx + (-v*dry-dvy*r      )*dtdy + (-w*drz-dwz*r      )*dtdz;
+    su0 = (-u*dux-dpx/r      )*dtdx + (-v*duy            )*dtdy + (-w*duz            )*dtdz;
+    sv0 = (-u*dvx            )*dtdx + (-v*dvy-dpy/r      )*dtdy + (-w*dvz            )*dtdz;
+    sw0 = (-u*dwx            )*dtdx + (-v*dwy            )*dtdy + (-w*dwz-dpz/r      )*dtdz;
+    sp0 = (-u*dpx-dux*gamma*p)*dtdx + (-v*dpy-dvy*gamma*p)*dtdy + (-w*dpz-dwz*gamma*p)*dtdz;
+
+    // Update in time the  primitive variables
+    r = r + sr0;
+    u = u + su0;
+    v = v + sv0;
+    w = w + sw0;
+    p = p + sp0;
+    
+    // reconstruct state on interface
+    qr[ID] = r + offsets[IX] * drx + offsets[IY] * dry + offsets[IZ] * drz ;
+    qr[IP] = p + offsets[IX] * dpx + offsets[IY] * dpy + offsets[IZ] * dpz ;
+    qr[IU] = u + offsets[IX] * dux + offsets[IY] * duy + offsets[IZ] * duz ;
+    qr[IV] = v + offsets[IX] * dvx + offsets[IY] * dvy + offsets[IZ] * dvz ;
+    qr[IW] = w + offsets[IX] * dwx + offsets[IY] * dwy + offsets[IZ] * dwz ;
+
+    qr[ID] = fmax(smallr, qr[ID]);
+
+    return qr;
+    
+  } // reconstruct_state_3d
+
+  // =======================================================================
+  // =======================================================================
   KOKKOS_INLINE_FUNCTION
   void operator_2d(const size_t i) const 
   {
@@ -645,12 +757,156 @@ public:
 
   } // operator_2d
 
+  // =======================================================================
+  // =======================================================================
   KOKKOS_INLINE_FUNCTION
   void operator_3d(const size_t i) const 
   {
   
+    constexpr int dim = 3;
+    // codim=1 ==> faces
+    // codim=2 ==> edges
+    const int codim = 1;
+    
+    // number of faces per cell
+    uint8_t nfaces = 2*dim;
+
+    const int nbvar = params.nbvar;
+
+    // current cell size
+    real_t dx = pmesh->getSize(i);
+    
+    // current cell volume
+    real_t dV = dx*dx*dx;
+
+    // this vector contains quad ids
+    // corresponding to neighbors
+    std::vector<uint32_t> neigh; // through a given face
+
+    // this vector contains ghost status of each neighbors
+    std::vector<bool> isghost; // through a given face
+
+    // current cell center state (primitive variables)
+    HydroState3d qprim;
+    for (uint8_t ivar=0; ivar<nbvar; ++ivar)
+      qprim[ivar] = Qdata(i,fm[ivar]);
+
+    // current cell conservative variable state
+    HydroState3d qcons;
+    for (uint8_t ivar=0; ivar<nbvar; ++ivar)
+      qcons[ivar] = Data_in(i,fm[ivar]);
+
+    // iterate neighbors through a given face
+    for (uint8_t iface = 0; iface < nfaces; ++iface) {
+      
+      // find neighbors Id
+      pmesh->findNeighbours(i, iface, codim, neigh, isghost);
+      
+      // sweep neighbors accross face identified by iface
+      for (uint16_t j = 0; j < neigh.size(); ++j) {
+
+        uint32_t i_n = neigh[j];
+
+        // 0. retrieve primitive variables in neighbor cell
+        HydroState3d qprim_n;
+        if (isghost[j]) {
+          for (uint8_t ivar = 0; ivar < nbvar; ++ivar)
+            qprim_n[ivar] = Qdata_ghost(i_n, fm[ivar]);
+        } else {
+          for (uint8_t ivar = 0; ivar < nbvar; ++ivar)
+            qprim_n[ivar] = Qdata(i_n, fm[ivar]);
+        }
+
+        // 1. reconstruct primitive variables on both sides of current interface (iface)
+
+        // current cell reconstruction  (primitive variables)
+        const real_t dx_over_2 = pmesh->getSize(i)/2;
+        const offsets_t offsets = get_reconstruct_offsets_current(i, i_n, isghost[j], iface);
+        HydroState3d qr_c = reconstruct_state_3d(qprim, i, false, offsets, dx_over_2, dt);
+
+        // neighbor cell reconstruction (primitive variables)
+        const real_t size_n = isghost[j] ? pmesh->getSizeGhost(i_n) : pmesh->getSize(i_n);
+        const real_t dx_over_2_n = size_n/2;
+        const offsets_t offsets_n = get_reconstruct_offsets_neighbor(i, i_n, isghost[j], iface);
+        HydroState3d qr_n = reconstruct_state_3d(qprim_n, i_n, isghost[j], offsets_n, dx_over_2_n, dt);
+
+        // 2. we now have "qleft / qright" state ready to solver Riemann problem
+        HydroState3d flux;
+
+        // riemann solver along Y direction requires to swap velocity
+        // components
+        if (face_along_axis<IY>(iface)) {
+          swap(qr_c[IU], qr_c[IV]);
+          swap(qr_n[IU], qr_n[IV]);
+        }
+
+        // riemann solver along Z direction requires to swap velocity
+        // components
+        if (face_along_axis<IZ>(iface)) {
+          swap(qr_c[IU], qr_c[IW]);
+          swap(qr_n[IU], qr_n[IW]);
+        }
+
+        if (iface==0 or iface==2 or iface==4) {
+
+          riemann_hydro(qr_n,qr_c,flux,params);
+
+        } else if (iface==1 or iface==3 or iface==5) {
+
+          riemann_hydro(qr_c,qr_n,flux,params);
+
+        }
+
+        // swap back velocity components in flux when dealing with 
+        // a face along IY direction
+        if (face_along_axis<IY>(iface)) {
+          swap(flux[IU], flux[IV]);
+        }
+        
+        // swap back velocity components in flux when dealing with 
+        // a face along IY direction
+        if (face_along_axis<IZ>(iface)) {
+          swap(flux[IU], flux[IW]);
+        }
+        
+        // 3. accumulate flux into qcons
+        
+        // current face area:
+        // if neighbor is smaller, flux is divided by the number of sub-faces
+        // else only one interface (neigh.size = 1)
+        real_t dS = dx*dx / neigh.size();
+        real_t scale = dt*dS/dV;
+
+        if (iface == 0 or iface == 2 or iface == 4) {
+          qcons[ID] += flux[ID]*scale;
+          qcons[IE] += flux[IE]*scale;
+          qcons[IU] += flux[IU]*scale;
+          qcons[IV] += flux[IV]*scale;
+          qcons[IW] += flux[IW]*scale;
+        }
+        if (iface == 1 or iface == 3 or iface == 5) {
+          qcons[ID] -= flux[ID]*scale;
+          qcons[IE] -= flux[IE]*scale;
+          qcons[IU] -= flux[IU]*scale;
+          qcons[IV] -= flux[IV]*scale;
+          qcons[IW] -= flux[IW]*scale;
+        }
+
+      } // end for j (neighbors accross a given face)
+
+    } // end for iface
+
+    // Now we can update current cell (write qcons into Data_out)
+    Data_out(i,fm[ID]) = qcons[ID];
+    Data_out(i,fm[IE]) = qcons[IE];
+    Data_out(i,fm[IU]) = qcons[IU];
+    Data_out(i,fm[IV]) = qcons[IV];
+    Data_out(i,fm[IW]) = qcons[IW];
+
   } // operator_3d
 
+  // =======================================================================
+  // =======================================================================
   KOKKOS_INLINE_FUNCTION
   void operator()(const size_t i) const
   {
