91590f93bda51c91e4672b7918b5462dd80a5f8c
Pierre Kestener <pierre.kestener@cea.fr>
2019-07-22 17:01:30 +0200

    typedef turned into a using

--- src/shared/kokkos_shared.h
+++ src/shared/kokkos_shared.h
@@ -19,9 +19,25 @@ enum KokkosLayout {
   KOKKOS_LAYOUT_RIGHT
 };
 
-// last index is hydro variable
-// n-1 first indexes are space (i,j,k,....)
-typedef Kokkos::View<real_t**, Device> DataArray;
-typedef DataArray::HostMirror          DataArrayHost;
+/**
+ * DataArray mostly used when using one cell per octree leaf.
+ *
+ * first index is leaf id (curvilinear index along the Morton curve)
+ * last index is hydro variable
+ */
+using DataArray     = Kokkos::View<real_t**, Device>;
+using DataArrayHost = DataArray::HostMirror;
+
+/**
+ * DataArrayBlock used when designing a solver with a block of
+ * data per leaf of the octree.
+ *
+ * first index identifies a cell inside a block (left layout, from 0 to bx by -1)
+ * second index identifies the variable (rho, momentum, energy, ...)
+ * third index is the leaf id (curvilinear index along the Morton curve)
+ *
+ * Note that we enforce Left layout here, since we plan to the Kokkos TeamPolicy with one team per leaf, so we favor memory locality inside a block.
+ */
+using DataArrayBlock = Kokkos::View<real_t***, Kokkos::LayoutLeft, Device>;
 
 #endif // KOKKOS_SHARED_H_
