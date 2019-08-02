742ff8393399ebfdc8f0a517204c7122d7f97e19
Pierre Kestener <pierre.kestener@cea.fr>
2019-08-02 16:47:09 +0200

    fisrt version of blast init condition for block AMR

--- src/muscl_block/init/InitBlast.h
+++ src/muscl_block/init/InitBlast.h
@@ -34,6 +34,9 @@ namespace dyablo { namespace muscl_block {
  */
 class InitBlastDataFunctor {
 
+private:
+ uint32_t nbTeams; //!< number of thread teams
+
 public:
   using team_policy_t = Kokkos::TeamPolicy<Kokkos::IndexType<int32_t>>;
   using thread_t = team_policy_t::member_type;
@@ -42,31 +45,42 @@ public:
                        HydroParams    params,
                        BlastParams    bParams,
                        id2index_t     fm,
+                       Kokkos::Array<int,3> blockSizes,
                        DataArrayBlock Udata) :
     pmesh(pmesh), params(params), bParams(bParams),
     fm(fm), Udata(Udata)
-  {};
+  {
+    bx = blockSizes[IX];
+    by = blockSizes[IY];
+    bz = blockSizes[IZ];
+  };
   
   // static method which does it all: create and execute functor
   static void apply(std::shared_ptr<AMRmesh> pmesh,
 		    HydroParams    params,
                     ConfigMap      configMap,
 		    id2index_t     fm,
+                    Kokkos::Array<int,3> blockSizes,
                     DataArrayBlock Udata)
   {
     BlastParams blastParams = BlastParams(configMap);
     
     // data init functor
-    InitBlastDataFunctor functor(pmesh, params, blastParams, fm, Udata);
+    InitBlastDataFunctor functor(pmesh, params, blastParams, fm, blockSizes, Udata);
+
+    // kokkos execution policy
+    nbTeams = configMap.getInteger("init","nbTeams",16);
+    team_policy_t policy (nbTeams,
+                          Kokkos::AUTO() /* team size chosen by kokkos */);
 
     Kokkos::parallel_for("dyablo::muscl_block::InitBlastDataFunctor",
-                         pmesh->getNumOctants(), functor);
+                         policy, functor);
   }
   
   KOKKOS_INLINE_FUNCTION
-  void operator()(const size_t& i) const
+  void operator()(team_policy_t::member_type member) const
   {
-    
+
     // blast problem parameters
     const real_t blast_radius      = bParams.blast_radius;
     const real_t radius2           = blast_radius*blast_radius;
@@ -80,36 +94,82 @@ public:
     
     const real_t gamma0            = params.settings.gamma0;
 
-    // get cell center coordinate in the unit domain
-    // FIXME : need to refactor AMRmesh interface to use Kokkos::Array
-    std::array<double,3> center = pmesh->getCenter(i);
-    
-    const real_t x = center[0];
-    const real_t y = center[1];
-    const real_t z = center[2];
-    
-    real_t d2 = 
-      (x-blast_center_x)*(x-blast_center_x)+
-      (y-blast_center_y)*(y-blast_center_y);  
+    uint32_t iOct = member.league_rank();
 
-    if (params.dimType == THREE_D)
-      d2 += (z-blast_center_z)*(z-blast_center_z);
+    unit32_t nbCells = bx*by*bz;
 
-    // if (d2 < radius2) {
-    //   Udata(i    , fm[ID]) = blast_density_in; 
-    //   Udata(i    , fm[IP]) = blast_pressure_in/(gamma0-1.0); 
-    //   Udata(i    , fm[IU]) = 0.0;
-    //   Udata(i    , fm[IV]) = 0.0;
-    // } else {
-    //   Udata(i    , fm[ID]) = blast_density_out; 
-    //   Udata(i    , fm[IP]) = blast_pressure_out/(gamma0-1.0); 
-    //   Udata(i    , fm[IU]) = 0.0;
-    //   Udata(i    , fm[IV]) = 0.0;
-    // }
+    while (iOct <  pmesh->getNumOctants() )
+    {
 
-    // if (params.dimType == THREE_D)
-    //   Udata(i, fm[IW]) = 0.0;
-    
+      // get octant size
+      const real_t octSize = pmesh->getSize(iOct);
+
+      const real_t cellSize = octSize/bx;
+
+      // get cell center coordinate in the unit domain
+      // FIXME : need to refactor AMRmesh interface to use Kokkos::Array
+      // std::array<double,3> center = pmesh->getCenter(iOct);
+      
+      // const real_t xc = center[0];
+      // const real_t yc = center[1];
+      // const real_t zc = center[2];
+
+      // coordinates of the lower left corner
+      const real_t xo = pmesh->getNode(iOct, 0)[0];
+      const real_t yo = pmesh->getNode(iOct, 0)[1];
+      const real_t zo = pmesh->getNode(iOct, 0)[2];
+
+
+      Kokkos::parallel_for(
+        Kokkos::TeamVectorRange(member, nbCells),
+        KOKKOS_LAMBDA(const int32_t index) {
+
+          // convert index into ix,iy,iz of local cell inside
+          // block
+          real_t ix, iy, iz;
+
+          if (params.dimType == TWO_D) {
+            iy = index / bx;
+            ix = index - bx * iy;
+          } else {
+            iz = index / (bx*by);
+            int32_t index2 = index - bx*by*iz; 
+            iy = index2 / bx;
+            ix = index2 - bx*iy;
+          }
+
+          // compute x,y,z for that cell (cell center)
+          real_t x = x0 + ix*cellSize + cellSize/2;
+          real_t y = y0 + iy*cellSize + cellSize/2;
+          real_t z = z0 + iz*cellSize + cellSize/2;
+
+          // initialize
+          real_t d2 = 
+            (x-blast_center_x)*(x-blast_center_x)+
+            (y-blast_center_y)*(y-blast_center_y);  
+          
+          if (params.dimType == THREE_D)
+            d2 += (z-blast_center_z)*(z-blast_center_z);
+
+          if (d2 < radius2) {
+            Udata(index, fm[ID], iOct) = blast_density_in;
+            Udata(index, fm[IP], iOct) = blast_pressure_in/(gamma0-1.0);
+            Udata(index, fm[IU], iOct) = 0.0;
+            Udata(index, fm[IV], iOct) = 0.0;
+          } else {
+            Udata(index, fm[ID], iOct) = blast_density_out;
+            Udata(index, fm[IP], iOct) = blast_pressure_out/(gamma0-1.0);
+            Udata(index, fm[IU], iOct) = 0.0;
+            Udata(index, fm[IV], iOct) = 0.0;
+          }
+
+          if (params.dimType == THREE_D)
+            Udata(index, fm[IW], iOct) = 0.0;
+
+        }); // end TeamVectorRange
+
+    } // end while
+
   } // end operator ()
 
   std::shared_ptr<AMRmesh> pmesh;
@@ -117,7 +177,10 @@ public:
   BlastParams    bParams;
   id2index_t     fm;
   DataArrayBlock Udata;
-  
+
+  // block size
+  int            bx,by,bz; 
+
 }; // InitBlastDataFunctor
 
 /*************************************************/
