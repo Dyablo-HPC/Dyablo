fe68ceaab33199826a0eda71e912bed97affb817
Pierre Kestener <pierre.kestener@cea.fr>
2019-10-03 12:06:57 +0200

    add operator += and* for HydroState2d/3d class

--- src/shared/HydroState.h
+++ src/shared/HydroState.h
@@ -11,21 +11,33 @@ constexpr int MHD_2D_NBVAR = 8;
 constexpr int MHD_3D_NBVAR = 8;
 constexpr int MHD_NBVAR = 8;
 
-template<uint32_t dim>
+template<size_t dim>
 using StateNd = Kokkos::Array<real_t, dim>;
 
-using HydroState2d = StateNd<(uint32_t)HYDRO_2D_NBVAR>;
-using HydroState3d = StateNd<(uint32_t)HYDRO_3D_NBVAR>;
+using HydroState2d = StateNd<HYDRO_2D_NBVAR>;
+using HydroState3d = StateNd<HYDRO_3D_NBVAR>;
 using MHDState = Kokkos::Array<real_t, MHD_NBVAR>;
 using BField = Kokkos::Array<real_t, 3>;
 
-template<uint32_t dim>
+template<size_t dim>
 KOKKOS_INLINE_FUNCTION
-StateNd<dim>& operator+=(StateNd<dim>& lhs, StateNd<dim>& rhs)
+StateNd<dim>& operator+=(StateNd<dim>& lhs, const StateNd<dim>& rhs)
 {
 
   for (size_t i=0; i<dim; ++i)
     lhs[i] += rhs[i];
+
+  return lhs;
+
+} // operator+=
+
+template<size_t dim>
+KOKKOS_INLINE_FUNCTION
+StateNd<dim>& operator*(StateNd<dim>& lhs, real_t rhs)
+{
+
+  for (size_t i=0; i<dim; ++i)
+    lhs[i] *= rhs;
 
   return lhs;
 
