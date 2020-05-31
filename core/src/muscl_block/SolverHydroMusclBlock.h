e4cb0c1211e6580fe6e6775d4a5848d362c2e5a1
Pierre Kestener <pierre.kestener@cea.fr>
2020-05-31 14:14:43 +0200

    typo

--- core/src/muscl_block/SolverHydroMusclBlock.h
+++ core/src/muscl_block/SolverHydroMusclBlock.h
@@ -98,7 +98,7 @@ public:
   //! hydrodynamics primitive - array of octant's block data
   DataArrayBlock Qgroup;
 
-  //! 2:1 intereface flags
+  //! 2:1 interface flags
   FlagArrayBlock Interface_flags;
 
   //! slopes along X dir - array of octant's block data
