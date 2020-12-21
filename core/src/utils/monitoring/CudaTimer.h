e594f8368dfa7a40dda61be9d7a611ef2b95d133
Arnaud Durocher <arnaud.durocher@cea.fr>
2020-12-21 11:37:19 +0100

    delay cuda timer CPU syncronization

--- core/src/utils/monitoring/CudaTimer.h
+++ core/src/utils/monitoring/CudaTimer.h
@@ -25,6 +25,7 @@ protected:
 
   //! total accumulated duration
   double total_time;
+  bool pending = false;
 
 public:
   CudaTimer()
@@ -45,6 +46,8 @@ public:
   //! start timer, push a start even in a cuda stream
   void start()
   {
+    if(pending)
+      sync();
     cudaEventRecord(startEv, 0);
   }
 
@@ -52,13 +55,27 @@ public:
   void reset()
   {
     total_time = 0.0;
+    pending = false;
   }
 
   //! stop timer and accumulate time in seconds
   void stop()
   {
-    float gpuTime;
     cudaEventRecord(stopEv, 0);
+    pending=true;
+  }
+
+  //! return elapsed time in seconds (as record in total_time)
+  double elapsed()
+  {
+    if(pending) sync();
+    return total_time;
+  }
+
+private:
+  void sync()
+  {
+    float gpuTime;
     cudaEventSynchronize(stopEv);
 
     // get elapsed time in milliseconds
@@ -66,12 +83,7 @@ public:
 
     // accumulate duration in seconds
     total_time += (double)1e-3*gpuTime;
-  }
-
-  //! return elapsed time in seconds (as record in total_time)
-  double elapsed() const
-  {
-    return total_time;
+    pending=false;
   }
 
 }; // class CudaTimer
