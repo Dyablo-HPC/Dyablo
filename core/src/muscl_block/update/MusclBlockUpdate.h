fb30788208a3ef63531c3643edc455f42ecfdf6d
Arnaud Durocher <arnaud.durocher@cea.fr>
2022-01-27 11:30:14 +0100

    Added MusclBlockUpdate_muscl_oneneighbor based on  godunov solver implementation

--- core/src/muscl_block/update/MusclBlockUpdate.h
+++ core/src/muscl_block/update/MusclBlockUpdate.h
@@ -7,6 +7,7 @@ namespace muscl_block {
 
 class MusclBlockUpdate_legacy;
 class MusclBlockUpdate_generic;
+class MusclBlockUpdate_muscl_oneneighbor;
 
 } //namespace dyablo 
 } //namespace muscl_block
@@ -16,6 +17,7 @@ inline bool dyablo::muscl_block::MusclBlockUpdateFactory::init()
 {
   DECLARE_REGISTERED(dyablo::muscl_block::MusclBlockUpdate_legacy);
   DECLARE_REGISTERED(dyablo::muscl_block::MusclBlockUpdate_generic);
+  DECLARE_REGISTERED(dyablo::muscl_block::MusclBlockUpdate_muscl_oneneighbor);
 
   return true;
 }
