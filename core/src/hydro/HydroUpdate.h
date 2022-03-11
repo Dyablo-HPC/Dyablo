6a2c9aebf793a50a6357e541723884590dc0bd53
Maxime <maxime.delorme@cea.fr>
2022-03-11 16:18:26 +0100

    Runge-Kutta 2 solver

--- core/src/hydro/HydroUpdate.h
+++ core/src/hydro/HydroUpdate.h
@@ -9,6 +9,7 @@ class HydroUpdate_legacy;
 class HydroUpdate_generic;
 class HydroUpdate_muscl_oneneighbor;
 class HydroUpdate_euler;
+class HydroUpdate_RK2;
 
 } //namespace dyablo 
 
@@ -20,6 +21,7 @@ inline bool dyablo::HydroUpdateFactory::init()
   DECLARE_REGISTERED(dyablo::HydroUpdate_generic);
   DECLARE_REGISTERED(dyablo::HydroUpdate_muscl_oneneighbor);
   DECLARE_REGISTERED(dyablo::HydroUpdate_euler);
+  DECLARE_REGISTERED(dyablo::HydroUpdate_RK2);
 
   return true;
 }
