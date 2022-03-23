efddc488547f852b774fcd3f75d105219ee9a455
Maxime <maxime.delorme@cea.fr>
2022-03-23 22:00:25 +0100

    Reversed Riemann solvers to previous state

--- core/src/RiemannSolvers.h
+++ core/src/RiemannSolvers.h
@@ -613,20 +613,20 @@ void riemann_hydro( const HydroState& qleft,
 		                const RiemannParams& params)
 {
   //assert(params.riemannSolverType == RIEMANN_HLLC);
-   switch(params.riemannSolverType){
+  // switch(params.riemannSolverType){
   //   case RIEMANN_APPROX:
   //     riemann_approx(qleft,qright,flux,params);
   //     break;
-     case RIEMANN_HLLC:
+  //   case RIEMANN_HLLC:
        riemann_hllc(qleft,qright,flux,params);
-       break;
-     case RIEMANN_HLL:
-       riemann_hll(qleft,qright,flux,params);
-       break;
+  //     break;
+  //   case RIEMANN_HLL:
+  //     riemann_hll(qleft,qright,flux,params);
+  //     break;
   //   case RIEMANN_LLF:
   //     riemann_llf(qleft,qright,flux,params);
   //     break;
-   }
+  // }
 } // riemann_hydro
 
 /**
