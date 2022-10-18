834cf893d4ee77bd1f5df8cbbefe323fcf9092be
Noe Brucy <noe.brucy@cea.fr>
2022-10-18 16:30:04 +0200

    Remove hardcoded G, use more generic names

--- core/src/gravity/GravitySolver_point_mass.cpp
+++ core/src/gravity/GravitySolver_point_mass.cpp
@@ -9,16 +9,21 @@ namespace dyablo{
 class AnalyticalFormula_gravity_point_mass : public AnalyticalFormula_gravity
 {
 private:
-  real_t star_x0, star_y0, star_z0;
-  real_t star_mass, softening;
+    
+  // Position of the central mass
+  real_t point_mass_x, point_mass_y, point_mass_z; 
+  // Gravity parameter of the central mass (gravitational constant G times the object mass)
+  real_t point_mass_Gmass;
+  // Gravitational softening, to avoid divergence of the gravitational force.
+  real_t softening;                                
 
 
 public:
   AnalyticalFormula_gravity_point_mass( ConfigMap& configMap )
-  : star_x0( configMap.getValue<real_t>("gravity", "star_x0",  0.5) ),
-    star_y0( configMap.getValue<real_t>("gravity", "star_y0",  0.5) ),
-    star_z0( configMap.getValue<real_t>("gravity", "star_z0",  0.5) ),
-    star_mass( configMap.getValue<real_t>("gravity", "star_mass", 1) ),
+  : point_mass_x( configMap.getValue<real_t>("gravity", "point_mass_x",  0.5) ),
+    point_mass_y( configMap.getValue<real_t>("gravity", "point_mass_y",  0.5) ),
+    point_mass_z( configMap.getValue<real_t>("gravity", "point_mass_z",  0.5) ),
+    point_mass_Gmass( configMap.getValue<real_t>("gravity", "point_mass_Gmass", 1) ),
     softening( configMap.getValue<real_t>("gravity", "softening",  0.005) )
   {}
 
@@ -26,21 +31,20 @@ public:
   GravityField_t value( real_t x, real_t y, real_t z, real_t dx, real_t dy, real_t dz ) const
   {
     // Centered coordinates
-    real_t rx = x - star_x0;
-    real_t ry = y - star_y0;
-    real_t rz = z - star_z0;
+    real_t rx = x - point_mass_x;
+    real_t ry = y - point_mass_y;
+    real_t rz = z - point_mass_z;
 
     // Square radius from central object (softened)
     real_t r2 = rx*rx + ry*ry + rz*rz + softening*softening;
 
     real_t r = sqrt(r2);
-    constexpr real_t G = 1.0 / (4.0 * M_PI); // Gravitational constant
-    real_t const_term = - G * star_mass / (r2 * r);
+    real_t const_term = - point_mass_Gmass / (r2 * r);
 
     return{
-      const_term * rx,
-      const_term * ry,
-      const_term * rz // This will not be written in 2D
+      const_term * rx, // gx
+      const_term * ry, // gy
+      const_term * rz  // gz (this will not be written in 2D)
     };
   }
 };
