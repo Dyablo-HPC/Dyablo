efeb7c2e0bf58da851d6112d058cadd8c3cad72f
Arnaud Durocher <arnaud.durocher@cea.fr>
2023-03-28 20:49:13 +0200

    CIC kernels for particle projection/interpolation for self gravity

--- core/src/particles/ParticleUpdate.h
+++ core/src/particles/ParticleUpdate.h
@@ -8,6 +8,8 @@ namespace dyablo {
 class ParticleUpdate_tracers_move;
 class ParticleUpdate_NGP_move;
 class ParticleUpdate_NGP_density;
+class ParticleUpdate_CIC_move;
+class ParticleUpdate_CIC_density;
 
 } //namespace dyablo 
 
@@ -18,6 +20,8 @@ inline bool dyablo::ParticleUpdateFactory::init()
   DECLARE_REGISTERED(dyablo::ParticleUpdate_tracers_move);
   DECLARE_REGISTERED(dyablo::ParticleUpdate_NGP_move);
   DECLARE_REGISTERED(dyablo::ParticleUpdate_NGP_density);
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_CIC_move);
+  DECLARE_REGISTERED(dyablo::ParticleUpdate_CIC_density);
 
   return true;
 }
